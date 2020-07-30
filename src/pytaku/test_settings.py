af8f5ac3bc6f61754f569ebe959527c4186aa2d6
Bùi Thành Nhân <hi@imnhan.com>
2020-07-30 16:24:02 +0700

    the big purge

--- src/pytaku/test_settings.py
+++
@@ -1,4 +0,0 @@
-from .settings import *  # noqa
-
-DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
-MEDIA_ROOT = "test_media"
--- src/pytaku_web/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Chapter, Title
-
-admin.site.register(Title)
-admin.site.register(Chapter)
