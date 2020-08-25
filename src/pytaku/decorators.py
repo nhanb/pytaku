48becd10fe783ae937cfcda772f38e88659a6ffb
Bùi Thành Nhân <hi@imnhan.com>
2020-08-25 21:15:15 +0700

    remove old stuff before SPA

--- src/pytaku/decorators.py
+++ src/pytaku/decorators.py
@@ -1,18 +1,8 @@
 from functools import wraps
 
-from flask import jsonify, redirect, request, session, url_for
+from flask import jsonify, request
 
-from .persistence import read, unread, verify_token
-
-
-def require_login(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user") is None:
-            return redirect(url_for("auth_view", next=request.url))
-        return f(*args, **kwargs)
-
-    return decorated_function
+from .persistence import verify_token
 
 
 def process_token(required=True):
@@ -48,39 +38,3 @@ def process_token(required=True):
         return decorated_function
 
     return decorator
-
-
-def toggle_has_read(f):
-    """
-    Augments a view with the ability to toggle a chapter's read status if there's a
-    `?has_read=<chapter_id>` url param.
-    """
-
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        assert "site" in kwargs
-        assert "title_id" in kwargs
-        has_read_chapter_id = request.args.get("has_read")
-        unread_chapter_id = request.args.get("unread")
-        assert not (has_read_chapter_id and unread_chapter_id)  # can't do both
-
-        if session.get("user"):
-            if has_read_chapter_id:
-                read(
-                    session["user"]["id"],
-                    kwargs["site"],
-                    kwargs["title_id"],
-                    has_read_chapter_id,
-                )
-                return redirect(request.url[: request.url.rfind("?")])
-            elif unread_chapter_id:
-                unread(
-                    session["user"]["id"],
-                    kwargs["site"],
-                    kwargs["title_id"],
-                    unread_chapter_id,
-                )
-                return redirect(request.url[: request.url.rfind("?")])
-        return f(*args, **kwargs)
-
-    return decorated_function
--- src/pytaku/main.py
+++ src/pytaku/main.py
@@ -6,20 +6,10 @@ from datetime import timedelta
 from typing import List, Tuple
 
 import requests
-from flask import (
-    Flask,
-    flash,
-    jsonify,
-    make_response,
-    redirect,
-    render_template,
-    request,
-    session,
-    url_for,
-)
+from flask import Flask, jsonify, make_response, render_template, request, url_for
 
 from .conf import config
-from .decorators import process_token, require_login
+from .decorators import process_token
 from .persistence import (
     create_token,
     delete_token,
@@ -71,136 +61,6 @@ def _chapter_name(chapter: dict):
     return result
 
 
-@app.route("/following", methods=["GET"])
-@require_login
-def follows_view():
-    titles = get_followed_titles(session["user"]["id"])
-    for title in titles:
-        thumbnail = title_thumbnail(title["site"], title["id"])
-        if title["site"] == "mangadex":
-            thumbnail = url_for("proxy_view", b64_url=_encode_proxy_url(thumbnail))
-        title["thumbnail"] = thumbnail
-    return render_template("old/follows.html", titles=titles)
-
-
-@app.route("/follow/<site>/<title_id>", methods=["POST"])
-@require_login
-def follow_view(site, title_id):
-    follow(session["user"]["id"], site, title_id)
-    return redirect(url_for("spa_title_view", site=site, title_id=title_id))
-
-
-@app.route("/unfollow/<site>/<title_id>", methods=["POST"])
-@require_login
-def unfollow_view(site, title_id):
-    unfollow(session["user"]["id"], site, title_id)
-    return redirect(url_for("spa_title_view", site=site, title_id=title_id))
-
-
-@app.route("/logout", methods=["POST"])
-def logout_view():
-    session.pop("user")
-    return redirect("/")
-
-
-@app.route("/auth", methods=["GET", "POST"])
-def auth_view():
-    if session.get("user"):
-        return redirect(url_for("home_view"))
-
-    if request.method == "POST":
-
-        if request.form["action"] == "register":
-            username = request.form["username"].strip()
-            password = request.form["password"]
-            confirm_password = request.form["confirm-password"]
-            message = None
-            if password != confirm_password:
-                message = "Password confirmation didn't match."
-                status_code = 400
-            elif not (username and password and confirm_password):
-                message = "Empty field(s) spotted. Protip: spaces don't count."
-                status_code = 400
-            elif (
-                len(username) < 2
-                or len(username) > 15
-                or len(password) < 5
-                or len(password) > 50
-            ):
-                message = "Invalid username/password length. Username length should be 2~15, password 5~50."
-                status_code = 400
-            else:  # success!
-                err = register_user(username, password)
-                if err:
-                    message = err
-                    status_code = 400
-                else:
-                    username = ""
-                    password = ""
-                    confirm_password = ""
-                    message = "Registration successful! You can login now."
-                    status_code = 200
-            return (
-                render_template(
-                    "old/auth.html",
-                    register_username=username,
-                    register_password=password,
-                    register_confirm_password=confirm_password,
-                    register_message=message,
-                    register_has_error=status_code != 200,
-                ),
-                status_code,
-            )
-
-        else:  # action == 'login'
-            username = request.form["username"].strip()
-            password = request.form["password"]
-            remember = request.form.get("remember") == "on"
-            if not (username and password):
-                message = "Empty field(s) spotted. Protip: spaces don't count."
-                status_code = 400
-            else:
-                user_id = verify_username_password(username, password)
-                if user_id is None:
-                    message = "Wrong username/password combination."
-                    status_code = 400
-                else:  # success!
-                    resp = redirect(request.args.get("next", url_for("home_view")))
-                    session.permanent = remember
-                    session["user"] = {"username": username, "id": user_id}
-                    return resp
-
-            return (
-                render_template(
-                    "old/auth.html",
-                    login_username=username,
-                    login_password=password,
-                    login_remember=remember,
-                    login_message=message,
-                    login_has_error=status_code != 200,
-                ),
-                status_code,
-            )
-
-    # Just a plain ol' GET request:
-    return render_template("old/auth.html")
-
-
-@app.route("/search")
-def search_view():
-    query = request.args.get("q", "").strip()
-    results = {}
-    if query:
-        results = search_title_all_sites(query)
-
-    if "mangadex" in results:
-        for title in results["mangadex"]:
-            title["thumbnail"] = url_for(
-                "proxy_view", b64_url=_encode_proxy_url(title["thumbnail"])
-            )
-    return render_template("old/search.html", results=results, query=query)
-
-
 @app.route("/proxy/<b64_url>")
 def proxy_view(b64_url):
     """Fine I'll do it"""
@@ -229,41 +89,6 @@ def _is_manga_img_url(
     ),
 ):
     return pattern.match(url)
-
-
-@app.route("/import", methods=["GET", "POST"])
-@require_login
-def import_view():
-    if request.method == "POST":
-
-        # check if the post request has the file part
-        if "tachiyomi" not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        file = request.files["tachiyomi"]
-
-        # if user does not select file, browser also
-        # submits an empty part without filename
-        if file.filename == "":
-            flash("No selected file")
-            return redirect(request.url)
-
-        if file:
-            text = file.read()
-            site_title_pairs = read_tachiyomi_follows(text)
-            if site_title_pairs is None:
-                flash("Malformed input file.")
-                return redirect(request.url)
-
-            # First fetch & save titles if they're not already in db
-            ensure_titles(site_title_pairs)
-
-            # Then follow them all
-            import_follows(session["user"]["id"], site_title_pairs)
-
-            flash(f"Added {len(site_title_pairs)} follows.")
-
-    return render_template("old/import.html")
 
 
 def read_tachiyomi_follows(text: str) -> List[Tuple[str, str]]:
@@ -329,11 +154,6 @@ def ensure_titles(site_title_pairs: List[Tuple[str, str]]):
             title_dicts.append(title)
 
     return title_dicts
-
-
-"""
-New Mithril-based SPA views follow
-"""
 
 
 @app.route("/")
